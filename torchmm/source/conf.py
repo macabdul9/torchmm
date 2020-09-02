8b6f61ffe0f9615399a6f44440bbe115e24bcfa3
macabdul9 <abdulwaheed1513@gmail.com>
2020-09-02 20:40:51 +0530

    utils dir added

--- torchmm/source/conf.py
+++ torchmm/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Abdul Waheed and Ganeshan Malhotra'
 author = 'Abdul Waheed and Ganeshan Malhotra'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.2 alpha'
 
 
 # -- General configuration ---------------------------------------------------
