a0ca607174a8f13bc1c409a58d8dcb21975f614e
macabdul9 <abdulwaheed1513@gmail.com>
2020-11-30 09:53:31 +0530

    Dataset Template

---
+++ torchmm/MMDataset.py
@@ -0,0 +1,26 @@
+from torch.utils.data import DataLoader, Dataset
+from datasets import load_dataset
+
+
+
+class MMDataset(Dataset):
+    
+    def __init__(self, config:None):
+        
+        
+        pass
+    
+    
+    def __str__(self):
+        pass
+    
+    def __getitem__(self):
+        pass
+    
+    def __len__(self):
+        pass
+
+
+if __name__=='__main__':
+    
+    print("hello world")
